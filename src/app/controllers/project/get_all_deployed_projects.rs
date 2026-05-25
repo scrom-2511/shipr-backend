5401dfdcc69ac9ab9d498f969676a60405787564
scrom-2511 <formain337@gmail.com>
2026-05-26 01:18:55 +0530

    add: get_all_deployed_projects controller

--- src/app/controllers/project/get_all_deployed_projects.rs
+++ src/app/controllers/project/get_all_deployed_projects.rs
@@ -4,41 +4,46 @@ use crate::app::middlewares::AuthMiddleware;
 use crate::app_errors::AppError;
 
 use actix_web::{HttpMessage, HttpRequest, HttpResponse, web};
+use chrono::NaiveDateTime;
 use serde::Serialize;
 use sqlx::FromRow;
 
 #[derive(Debug, Serialize, FromRow)]
 struct DeployedProject {
     id: i32,
+    branch: String,
     name: String,
-    url: String,
-    last_deployment_time: chrono::DateTime<chrono::Utc>,
-    repo_name: String,
+    full_name: String,
+    status: String,
+    last_deployment_time: NaiveDateTime,
 }
 
-#[derive(Debug, Serialize, FromRow)]
+#[derive(Debug, Serialize)]
 struct GetAllProjectsResponse {
     projects: Vec<DeployedProject>,
 }
 
-pub async fn get_all_projects_controller(
+pub async fn get_all_deployed_projects_controller(
     pool: web::Data<DbPool>,
     req: HttpRequest,
 ) -> Result<HttpResponse, AppError> {
+    println!("get_all_deployed_projects_controller called");
+
     let user_id = req.extensions().get::<AuthMiddleware>().unwrap().user_id;
+
+    println!("user_id: {}", user_id);
 
     let query = r#"
     SELECT
-        p.id,
-        p.name,
-        p.url,
-        p.last_deployment_time,
-        gr.repo_name
-    FROM projects p
-    JOIN github_repos gr
-    ON p.repo_id = gr.id
-    WHERE p.user_id = $1
-    ORDER BY p.created_at DESC
+        id,
+        branch,
+        name,
+        full_name,
+        status,
+        last_deployment_time
+    FROM projects
+    WHERE user_id = $1
+    ORDER BY created_at DESC
     "#;
 
     let projects: Vec<DeployedProject> = sqlx::query_as(query)
