e3398c4f5b554e9cad4809aee54ad2d508ab61fb
scrom-2511 <formain337@gmail.com>
2026-08-27 00:27:09 +0530

    fix: db queries

--- src/app/controllers/github/update_userid_github_app_installations.rs
+++ src/app/controllers/github/update_userid_github_app_installations.rs
@@ -1,12 +1,16 @@
-use actix_web::{web, HttpMessage, HttpRequest, HttpResponse};
-use sea_orm::{ConnectionTrait, DatabaseBackend, Statement};
+use actix_web::{HttpMessage, HttpRequest, HttpResponse, web};
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, DatabaseBackend, EntityTrait, QueryFilter, Set, Statement,
+    sea_query::Expr,
+};
 use serde::Deserialize;
 
 use crate::{
     app::{
-        controllers::{auth::decode_token, ApiResponse},
+        controllers::{ApiResponse, auth::decode_token},
         db::DbPool,
         middlewares::AuthMiddleware,
+        models::github_repos,
     },
     app_errors::AppError,
 };
@@ -22,28 +26,51 @@ pub async fn update_userid_github_app_installations(
     pool: web::Data<DbPool>,
     body: web::Json<UpdateUserIdGithubAppInstallationsRequest>,
 ) -> Result<HttpResponse, AppError> {
-    println!("body: {:?}", body);
     let user_id = req.extensions().get::<AuthMiddleware>().unwrap().user_id;
 
+    println!("hi there 1");
+
     let decoded_state = decode_token(&body.state)?;
+
+    println!("hi there 2");
 
     if decoded_state.user_id != user_id {
         return Err(AppError::InvalidCredentials);
     }
 
-    let stmt = Statement::from_sql_and_values(
-        DatabaseBackend::Postgres,
-        "UPDATE github_repos SET user_id = $1 WHERE $2 = ANY(installation_ids)",
-        vec![user_id.into(), body.installation_id.into()],
-    );
+    println!("hi there 3");
 
-    pool.execute(stmt)
+    let repo = match github_repos::Entity::find()
+        .from_raw_sql(Statement::from_sql_and_values(
+            DatabaseBackend::Postgres,
+            "SELECT * FROM github_repos WHERE $1 = ANY(installation_ids)",
+            vec![body.installation_id.into()],
+        ))
+        .one(pool.as_ref())
         .await
-        .map_err(|_| AppError::InternalServerError)?;
+    {
+        Ok(repo) => repo,
+        Err(e) => {
+            println!("Installation id update failed: {}", e);
+            return Err(AppError::Database(e.to_string()));
+        }
+    };
 
-    Ok(HttpResponse::Ok().json(ApiResponse::<()> {
-        success: true,
-        message: "User ID updated successfully".to_string(),
-        data: None,
-    }))
+    let repo = repo.ok_or_else(|| AppError::Database("Installation not found".to_string()))?;
+
+    let mut active_repo: github_repos::ActiveModel = repo.into();
+
+    active_repo.user_id = Set(Some(user_id));
+
+    match active_repo.update(pool.as_ref()).await {
+        Ok(_) => Ok(HttpResponse::Ok().json(ApiResponse::<()> {
+            success: true,
+            message: "User ID updated successfully".to_string(),
+            data: None,
+        })),
+        Err(e) => {
+            println!("Installation id update failed: {}", e);
+            Err(AppError::Database(e.to_string()))
+        }
+    }
 }
